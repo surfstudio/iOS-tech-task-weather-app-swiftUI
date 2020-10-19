3aa9eb03e6af1a4711c11002b62532799d39742e
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2020-10-19 18:38:50 +0300

    IDPT-497 cell

---
+++ Weather/Flows/Cities/CitiesList/View/Cells/AddCityCell.swift
@@ -0,0 +1,21 @@
+//
+//  AddCityCell.swift
+//  Weather
+//
+//  Created by Александр Кравченков on 19.10.2020.
+//
+
+import UIKit
+import ReactiveDataDisplayManager
+
+final class AddCityCell: UITableViewCell {
+
+}
+
+// MARK: - Configure Appearence
+
+private extension AddCityCell {
+    func configureAppearence() {
+        
+    }
+}
